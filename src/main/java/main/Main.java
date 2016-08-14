842ad37cf6a05fb61e30efb010a51556b1144dc1
Saahil Claypool <saahil@tracer.home>
2016-08-14 12:20:05 -0400

    gradle gradle gradle

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+import javax.swing.JFrame;
+
+class Main 
+{
+    public static void main(String args[])
+    {
+	System.out.printf("Hello World!%n");
+	JFrame  j = new JFrame();
+	j.setSize(500,500);
+	j.setVisible(true);
+	
+	
+	
+
+    }
+}
